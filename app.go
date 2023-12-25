7590ee984f39b1ef5c21b9126cc04e1fd4000273
SuperSolik <serpetr99@gmail.com>
2023-12-25 22:09:15 +0100

    init echo + templ

--- app.go
+++ app.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"net/http"
+	"supersolik/greed/views"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		err := views.Index("Sergei").Render(context.Background(), c.Response().Writer)
+
+		if err != nil {
+			return c.String(http.StatusInternalServerError, "unable to render template")
+		}
+
+		return nil
+	})
+	e.Logger.Fatal(e.Start("127.0.0.1:8080"))
 }
