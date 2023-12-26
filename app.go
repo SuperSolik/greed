064c46d2f9bc1e9b21af360684ac525dcb00f2c3
SuperSolik <serpetr99@gmail.com>
2023-12-26 21:04:42 +0100

    accounts table + edit using htmx

--- app.go
+++ app.go
@@ -2,22 +2,100 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"strconv"
+	"supersolik/greed/services"
 	"supersolik/greed/views"
 
+	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
 )
+
+func renderTempl(c echo.Context, t templ.Component) error {
+	return t.Render(context.Background(), c.Response().Writer)
+}
+
+var accounts = map[uint]services.Account{
+	1: {Id: 1, Name: "Visa Card", Currency: "RSD", Amount: 10_000},
+	2: {Id: 2, Name: "Cash", Currency: "EUR", Amount: 2_000},
+}
+
+func ExtractValues[K comparable, V any](m map[K]V) []V {
+	var values []V
+	for _, value := range m {
+		values = append(values, value)
+	}
+
+	return values
+}
 
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		err := views.Index("Sergei").Render(context.Background(), c.Response().Writer)
+		err := renderTempl(c, views.Index(ExtractValues(accounts)))
 
 		if err != nil {
 			return c.String(http.StatusInternalServerError, "unable to render template")
 		}
 
-		return nil
+		return err
+	})
+
+	e.GET("/accounts/:id", func(c echo.Context) error {
+		accountId, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+			return c.String(http.StatusBadRequest, fmt.Sprintf("Unable to parse %v to account id", accountId))
+		}
+
+		var edit bool
+
+		editParam := c.QueryParam("edit")
+
+		if editParam == "true" {
+			edit = true
+		} else {
+			edit = false
+		}
+
+		account := accounts[uint(accountId)]
+
+		err = renderTempl(c, views.Account(account, edit))
+
+		if err != nil {
+			return c.String(http.StatusInternalServerError, "unable to render template")
+		}
+
+		return err
+	})
+
+	e.POST("/accounts/:id/save", func(c echo.Context) error {
+		accountId, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+			return c.String(http.StatusBadRequest, fmt.Sprintf("Unable to parse %v to account id", accountId))
+		}
+
+		account := accounts[uint(accountId)]
+
+		amount := c.FormValue("amount")
+		parsedAmount, err := strconv.ParseFloat(amount, 32)
+
+		if err != nil {
+			return c.String(http.StatusNotAcceptable, fmt.Sprintf("Provided amount=%v is not valid numeric value", amount))
+		}
+
+		account.Update(c.FormValue("account_name"), float32(parsedAmount), c.FormValue("description"))
+		accounts[uint(accountId)] = account
+
+		err = renderTempl(c, views.Account(account, false))
+
+		if err != nil {
+			return c.String(http.StatusInternalServerError, "Unable to render template")
+		}
+
+		return err
 	})
 	e.Logger.Fatal(e.Start("127.0.0.1:8080"))
 }
---
+++ services/accounts.go
@@ -0,0 +1,15 @@
+package services
+
+type Account struct {
+	Id          uint
+	Name        string
+	Amount      float32
+	Currency    string
+	Description string
+}
+
+func (acc *Account) Update(name string, amount float32, description string) {
+	acc.Name = name
+	acc.Amount = amount
+	acc.Description = description
+}
